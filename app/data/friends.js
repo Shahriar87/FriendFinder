b0ba6fa852cfd9df7f73929641ae3be051b8cb25
Innovator <shahriar.firoz@gmail.com>
2018-11-14 02:23:57 -0600

    setting API route

---
+++ app/data/friends.js
@@ -0,0 +1,69 @@
+
+var friendsArray = [
+    {
+        name: "Sarah",
+        photo: "https://media.vanityfair.com/photos/57fe47016d65c9f44d47fdcb/master/w_768,c_limit/sarah-paulson-richard-lawson-interview.jpg",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        name: "Shah",
+        photo: "https://avatars3.githubusercontent.com/u/40048812?s=460&v=4",
+        scores: [
+            4,
+            2,
+            5,
+            1,
+            3,
+            2,
+            2,
+            1,
+            3,
+            2
+        ]
+    },
+    {
+        name: "Ryan Gosling",
+        photo: "https://www.aceshowbiz.com/images/photo/ryan_gosling.jpg",
+        scores: [
+            5,
+            2,
+            2,
+            2,
+            4,
+            1,
+            3,
+            2,
+            5,
+            5
+        ]
+    },
+    {
+        name: "Emma Stone",
+        photo: "https://www.t-nation.com/system/publishing/articles/10005364/original/Emma-Stone-Gains-18-Pounds-of-Muscle.jpg?1506027837",
+        scores: [
+            3,
+            3,
+            4,
+            2,
+            2,
+            1,
+            3,
+            2,
+            2,
+            3
+        ]
+    }
+];
+
+module.exports = friendsArray;
